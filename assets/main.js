83e8d6a7e9fcbbdbdb6863fe509d80dcf5e6ba51
Riccardo <riky.volto@live.com>
2023-08-22 14:51:35 +0200

    add structure and prompt on js

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+/* Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato,
+che va formattato in questo modo: nomecognomecolorepreferito23 
+Esempio: fabiopacificiblue23 */
+
+
+/* Come agire? */
+
+/* 
+-creare un prompt chiedendo il nome dell' utente 
+-creare un secondo prompt chiedendo il cognome
+-creare un terzo prompt chiedendo il colore preferito
+-stampare il risultato in console con console.log
+-stampare il risultato in pagina con Element.innerHTML
+*/
+
+/* prompt nome utente */
+
+const userName = prompt ('inserisci il tuo nome')
+
+/* prompt cognome */
+
+const userSourname = prompt ('inserisci il tuo cognome')
+
+/* prompt colore preferito */
+
+const favouriteColor = prompt ('inserisci il tuo colore preferito')
